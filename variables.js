575b270066755a129eb92216909a56e6aaba0f90
miniproject <sharat.hegde@sjsu.edu>
2016-09-19 02:00:56 -0700

    commit1
    html and javascript files

---
+++ variables.js
@@ -0,0 +1,23 @@
+<html>
+<head>
+<body>
+<p> here i shall demonstarte the use of variables </p>
+<script>
+var name = "";
+var feet = 6;
+var inches = 2;
+
+getElementById(name).innerHTML="sharat";
+
+document.write("My name is :" + name);
+
+var height = function (){
+	return feet + " \" . " + inches + "'";
+	
+}
+
+document.write (" My height is:"+ height);
+
+<script>
+</body>
+</html>
